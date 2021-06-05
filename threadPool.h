4c8cdf6128db12dcd96bd3cffb3f6cfb9462cb1f
ofek <ofekz1997@gmail.com>
2021-06-05 15:15:24 +0300

    add threadPool

---
+++ threadPool.h
@@ -0,0 +1,23 @@
+#ifndef THREAD_POOL_H_
+#define THREAD_POOL_H_
+
+#include "list.h"
+#include <pthread.h>
+#include "request.h"
+#include "segel.h"
+
+typedef struct Pool_t* ThreadPool;
+
+typedef enum SchedAlg_t {
+    BLOCK,
+    DROP_TAIL,
+    DROP_HEAD,
+    RANDOM_DROP
+} SchedAlg;
+
+ThreadPool ThreadPoolCreate(size_t poolSize, size_t maxRequest, SchedAlg schedAlg);
+void ThreadPoolDestroy(ThreadPool pool);
+void ThreadPoolAddRequest(ThreadPool pool,int fd);
+
+
+#endif // THREADS_POOL_H_
